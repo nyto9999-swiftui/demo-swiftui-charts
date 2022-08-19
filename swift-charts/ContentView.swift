052ea0911d6540c5bf4350f6df587522e5fb773e
ninjachen <nyto9999@gmail.com>
2022-08-19 17:48:32 +0800

    Initial Commit

---
+++ swift-charts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-charts
+//
+//  Created by 宇宣 Chen on 2022/8/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-charts/swift_chartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_chartsApp.swift
+//  swift-charts
+//
+//  Created by 宇宣 Chen on 2022/8/19.
+//
+
+import SwiftUI
+
+@main
+struct swift_chartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
